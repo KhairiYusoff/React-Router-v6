41a689559a77e98b1d021096a42894e17a3fd0b1
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-05-27 01:06:55 +0800

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./pages/Home"
+import Products from "./pages/Products"
+import Root from "./pages/Root"
 
 function App() {
 
   const router = createBrowserRouter([
-    { path: '/', element: <Home /> }
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/products', element: <Products /> },
+      ]
+    },
   ])
 
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
-        <div>My Home</div>
+        <>
+            <h1>My Homepage</h1>
+            <p>Go to <Link to="/products">the list of products</Link></p>
+
+        </>
     )
 }
 
